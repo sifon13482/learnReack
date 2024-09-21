7eb796a4a1f95c0fdedeae38a6748cca8e7f4a7b
Mikhail <sifon13482@gmail.com>
2024-09-21 16:09:40 +0300

    deafault

--- src/main.jsx
+++ src/main.jsx
@@ -2,21 +2,5 @@ import { createRoot } from "react-dom/client";
 import { restaurants } from "./materials/mock";
 
 createRoot(document.getElementById("root")).render(
-  restaurants.map((restaurant) => (
-    <div>
-      {restaurant.name}
-      <h3>Меню</h3>
-      <ul>
-        {restaurant.menu.map((dish) => (
-          <li>{dish.name}</li>
-        ))}
-      </ul>
-      <h3>Отзывы</h3>
-      <ul>
-        {restaurant.reviews.map((comment) => (
-          <li>{comment.text}</li>
-        ))}
-      </ul>
-    </div>
-  ))
+
 );
